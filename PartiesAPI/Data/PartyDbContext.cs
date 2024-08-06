664e0abf75c3252b8581dffc4f73558a6002d088
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-06 17:50:19 +0300

    Add PartyDbContext and it's DbSets

---
+++ PartiesAPI/Data/PartyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartiesAPI.Models;
+
+namespace PartiesAPI.Data
+{
+    public class PartyDbContext : DbContext
+    {
+        public PartyDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
