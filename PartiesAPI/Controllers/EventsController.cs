229cae40abcd7db6b7ba576c0ffd142ae6610fc9
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-06 22:31:30 +0300

    Add create event endpoint

--- PartiesAPI/Controllers/EventsController.cs
+++ PartiesAPI/Controllers/EventsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Security;
+using PartiesAPI.Data;
+using PartiesAPI.Models;
 
 namespace PartiesAPI.Controllers
 {
@@ -7,6 +10,26 @@ namespace PartiesAPI.Controllers
     [ApiController]
     public class EventsController : ControllerBase
     {
-        
+        private PartyDbContext _context;
+
+        public EventsController(PartyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateEvent(Event ev)
+        {
+            try
+            {
+                _context.Events.Add(ev);
+                _context.SaveChanges();
+                return Ok(ev);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
