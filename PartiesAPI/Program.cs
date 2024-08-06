763e6a93e54f258c018c2ab68bd3d6c9d5094f10
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-06 18:28:34 +0300

    Add connection string and dbcontext to builder

--- PartiesAPI/Program.cs
+++ PartiesAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PartiesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DbConnectionString");
+builder.Services.AddDbContext<PartyDbContext>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
 
