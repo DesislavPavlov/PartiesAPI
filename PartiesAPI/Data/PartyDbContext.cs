31394bf6394f93a7f60d0ed7206963db7c7e9f35
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-06 18:15:17 +0300

    Add constraints and relationships via Fluent API

--- PartiesAPI/Data/PartyDbContext.cs
+++ PartiesAPI/Data/PartyDbContext.cs
@@ -9,5 +9,18 @@ namespace PartiesAPI.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Event> Events { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var usersTable = modelBuilder.Entity<User>();
+            usersTable.HasKey(u => u.Id);
+            usersTable.Property(u => new { u.FirstName, u.LastName, u.Email }).IsRequired();
+
+            var eventsTable = modelBuilder.Entity<Event>();
+            eventsTable.HasKey(e => e.Id);
+            eventsTable.Property(e => new {e.Name, e.Location, e.StartTime, e.EndTime, e.Organizer}).IsRequired();
+            eventsTable.HasMany(e => e.Participants);
+        }
     }
 }
