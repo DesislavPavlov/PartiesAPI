8e2cf925dc8494ebd41f87f783bfb35c77eebe2e
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-06 16:32:31 +0300

    Add Event model

---
+++ PartiesAPI/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartiesAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Location { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set;}
+
+        [Required]
+        public DateTime EndTime { get; set;}
+
+        [Required]
+        public string? Organizer { get; set; }
+
+        public ICollection<User>? Participants { get; set; }
+    }
+}
