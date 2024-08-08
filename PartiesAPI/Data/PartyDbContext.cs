25ce9c3b7ed60052c0beb93592065d63b7b05c32
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-08-08 15:11:56 +0300

    Fix DbContext and Fluent API's structures

--- PartiesAPI/Data/PartyDbContext.cs
+++ PartiesAPI/Data/PartyDbContext.cs
@@ -9,18 +9,33 @@ namespace PartiesAPI.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Event> Events { get; set; }
+        public DbSet<EventParticipant> EventParticipants { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             var usersTable = modelBuilder.Entity<User>();
             usersTable.HasKey(u => u.Id);
-            usersTable.Property(u => new { u.FirstName, u.LastName, u.Email }).IsRequired();
+            usersTable.Property(u => u.FirstName).IsRequired();
+            usersTable.Property(u => u.LastName).IsRequired();
+            usersTable.Property(u => u.Email).IsRequired();
 
             var eventsTable = modelBuilder.Entity<Event>();
             eventsTable.HasKey(e => e.Id);
-            eventsTable.Property(e => new {e.Name, e.Location, e.StartTime, e.EndTime, e.Organizer}).IsRequired();
-            eventsTable.HasMany(e => e.Participants).WithMany(u => u.Events);
+            eventsTable.Property(e => e.Name).IsRequired();
+            eventsTable.Property(e => e.Location).IsRequired();
+            eventsTable.Property(e => e.StartDate).HasColumnType("datetime").IsRequired();
+            eventsTable.Property(e => e.EndDate).HasColumnType("datetime").IsRequired();
+            eventsTable.Property(e => e.Organizer).IsRequired();
+
+            var eventParticipantsTable = modelBuilder.Entity<EventParticipant>();
+            eventParticipantsTable.HasKey(ep => ep.Id);
+            eventParticipantsTable.Property(ep => ep.UserId).IsRequired();
+            eventParticipantsTable.Property(ep => ep.EventId).IsRequired();
+            eventParticipantsTable.HasOne(ep => ep.Event).WithMany().HasForeignKey(ep => ep.EventId);
+            eventParticipantsTable.HasOne(ep => ep.User).WithMany().HasForeignKey(ep => ep.UserId);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
